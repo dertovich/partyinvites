d56a79c739438bbc9c2dfa9a3dd34289146314d9
dertovich <huni_19@mail.ru>
2023-02-18 16:05:04 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Rsvp struct {
+	Name  string
+	Email string
+	Phone string
+
+	WillAttened bool
+}
+
+func main() {
+	fmt.Println("TO DO some features")
+}
