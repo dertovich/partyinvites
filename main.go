e176a83f11957af3d3e6905db96afecf6c9897e3
dertovich <huni_19@mail.ru>
2023-02-18 17:59:46 +0300

    HTML loaded

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 )
 
 type Rsvp struct {
@@ -13,7 +14,22 @@ type Rsvp struct {
 }
 
 var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+func loadTemplates() {
+	templatesNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+
+	for index, name := range templatesNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("TO DO some features")
+	loadTemplates()
 }
